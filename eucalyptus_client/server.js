0fe3ef10b056547abc719ffccbadabb21e89d8be
Harry A. Swan <has.land@hotmail.co.uk>
2016-07-08 12:12:13 +0100

    new client server

--- eucalyptus_client/server.js
+++ eucalyptus_client/server.js
@@ -3,10 +3,18 @@ var app = express();
 var path = require('path');
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/home/index.html'));
+});
+app.get('/:sitename', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/user/index.html'));
+});
+app.get('/:sitename/admin', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/admin/index.html'));
 });
 
-app.use(express.static('client/build'));
+app.use("static/home", express.static('./client/build/home'));
+app.use("static/admin", express.static('./client/build/admin'));
+app.use("static/user", express.static('./client/build/user'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
