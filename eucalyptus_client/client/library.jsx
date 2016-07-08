a5652036f25889cb5541df915f4981d21dba12bc
Harry A. Swan <has.land@hotmail.co.uk>
2016-07-08 17:09:30 +0100

    basic page load

--- eucalyptus_client/client/library.jsx
+++ eucalyptus_client/client/library.jsx
@@ -1,23 +1,34 @@
+var React = require('react');
+
 module.exports = {
 
-  getRequest: function (url, reqtype, callback, data=null) {
-    var request = new XMLHttpRequest();
-    request.open(reqtype, "http://localhost:5000/" + url);
-    if(reqtype != "GET" || reqtype != "get"){
-      request.setRequestHeader("Content-Type", "Application/json");
-    }
-    request.onload = function(){
-      if(request.status===200){
-        callback(JSON.parse(request.responseText));
-      }
-    }
-    request.send(data);
-  },
+    request: function(reqtype, url, data=null) {
+        return new Promise(function(resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.onload = function() {
+                if (request.status === 200) {
+                    resolve(JSON.parse(request.responseText));
+                } else {
+                    reject(request.status);
+                }
+            }
+            request.open(reqtype, "http://localhost:5000/" + url);
+            if (reqtype !== "get" && reqtype !== "GET") {
+                request.setRequestHeader('Content-Type', 'application/json');
+            }
+            request.send(data);
+        });
+    },
 
-  getSiteName: function (url) {
-    let urlparts = url.split("/");
-    let sitename = urlparts[3];
-    return sitename;
-  }
+    generateHTML: function(data) {
+        var el = React.createElement(data.etype, null, data.content);
+        return el;
+    },
+
+    getSiteName: function (url) {
+        let urlparts = url.split("/");
+        let sitename = urlparts[3];
+        return sitename;
+    }
 
 }
--- eucalyptus_client/client/src_user/components/UserBox.jsx
+++ eucalyptus_client/client/src_user/components/UserBox.jsx
@@ -2,26 +2,54 @@ var React = require('react');
 var Koala = require('../../library.jsx');
 
 var UserBox = React.createClass({
-  getInitialState: function() {
-    return {
-      info: null,
-      sitename: null,
-      page: null
-    };
-  },
+    getInitialState: function() {
+        return {
+          info: null,
+          page: null,
+          elements: []
+        };
+    },
 
-  componentDidMount: function() {
-    var url = this.props.sitename + "/general/1";
-    Koala.getRequest(url, "GET", function(data){
-      this.setState({info: data, sitename: this.props.sitename})
-    }.bind(this));
-  },
+    componentDidMount: function() {
+        var url = this.props.sitename + "/";
+        // var elements = [];
+        Koala.request("get", url + "general/1")
+        .then(function(data) {
+            this.setState({info: data[0], page: data[0].index})
+            Koala.request("get", url + "pages/" + data[0].index)
+            .then(function(data) {
+                var elements_id = data[0].elements_id
+                for (var element_id of elements_id) {
+                    Koala.request("get", url + "elements/" + element_id)
+                    .then(function(data) {
+                        var elements = this.state.elements;
+                        console.log('before', elements);
+                        elements.push(Koala.generateHTML(data[0]));
+                        this.setState({elements: elements}, function() {
+                            console.log('after1',this.state.elements);
+                        }.bind(this));
+                        console.log('after2',this.state.elements);
 
-  render: function() {
-    return (
-      <p>{this.state.info}</p>
-    );
-  }
+                    }.bind(this));
+                }
+
+            }.bind(this));
+        }.bind(this));
+        // .catch(function(error) {
+        //     console.error(error);
+        // });
+    },
+
+    render: function() {
+        var elements = this.state.elements;
+        
+        return (
+            <div>
+                <p>{this.props.sitename}</p>
+                {this.state.elements}
+            </div>
+        );
+    }
 
 });
 
