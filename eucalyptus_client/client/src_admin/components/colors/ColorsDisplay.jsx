b81dead4c810b17aa6d0bed4876eb8f6b2f1eaf1
Zak Buys <zakbuys85@gmail.com>
2016-07-11 14:55:19 +0100

    displays all color palettes

--- eucalyptus_client/client/src_admin/components/colors/ColorsDisplay.jsx
+++ eucalyptus_client/client/src_admin/components/colors/ColorsDisplay.jsx
@@ -4,9 +4,7 @@ var Koala = require('../../../library.jsx');
 var ColorsDisplay = React.createClass({
     getInitialState: function() {
         return {
-            allPalettes:[],
-            // name: "",
-            // colors:[]
+            allPalettes:[]
         };
     },
 
@@ -18,41 +16,44 @@ var ColorsDisplay = React.createClass({
         Koala.request("GET", this.props.site+"/colorschemes")
         .then(function(data) {
             console.log(data);
-            // this.setState({colors: data[0].colors});
-            // this.setState({name: data[0].name});
             this.setState({allPalettes: data});
         }.bind(this));
     },
 
     render: function() {
-        // var boxesofcolor = this.state.allPalettes.map(function(palette){
-        //     // var paletteName = palette.name
-        //     <h5>palette.name</h5>
-        //     // palette.colors.map(function(color){
-        //     //         var divStyle = {
-        //     //             background: color
-        //     //         }
-        //     //         console.log(color);
-        //     //         return(
-        //     //             <div className="color-div" style={divStyle}></div>
-        //     //             )
-        //     // });
-        // });
-
-        // var boxesofcolor = this.state.colors.map(function(color, index){
-        //                         var divStyle = {
-        //                             background: color
-        //                         }
-        //                         return (
-        //                             <div className="color-div" style={divStyle} key={index}></div>
-        //                             )
-        //                     }.bind(this))
+        var boxesofcolor = this.state.allPalettes.map(function(palette){
+            var divStyle1 = {
+                background: palette.background
+            }
+            var divStyle2 = {
+                background: palette.headerBackground
+            }
+            var divStyle3 = {
+                background: palette.headerText
+            }
+            var divStyle4 = {
+                background: palette.text
+            }
+            var divStyle5 = {
+                background: palette.feature
+            }
+            var paletteName = palette.name
+            return (
+                <div>
+                    <h5>{paletteName}</h5>
+                    <div className="color-div" style={divStyle1}></div>
+                    <div className="color-div" style={divStyle2}></div>
+                    <div className="color-div" style={divStyle3}></div>
+                    <div className="color-div" style={divStyle4}></div>
+                    <div className="color-div" style={divStyle5}></div>
+                </div>
+             )
+        });
 
         return (
             <div>
                 <h3>Lots of pretty colors below!</h3>
-                <h4>{this.state.name}</h4>
-                {/*{boxesofcolor}*/}
+                {boxesofcolor}
             </div>
         );
     }
